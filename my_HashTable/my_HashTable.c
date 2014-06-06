61da032a7ef549e621497891ab36fe2e4fff4bbf
Zhao Shulin <zhaoshulin.cn@gmail.com>
2014-06-06 11:08:07 +0800

    first commit

---
+++ my_HashTable/my_HashTable.c
@@ -0,0 +1,10 @@
+/* 
+ * Author: Zhao Shulin
+ * Date: 2014/06/06
+ * Purpose: A simple implemetation of HashTable in c. 
+ * 主要思路：我只要实现2个数据结构+5个接口即可：
+ *              数据结构：(1)hashnode_struct (2)hashtable_struct
+ *              接口：(1)hashtable_init() (2)hashtable_insert(key, value) (3)hashtable_remove(key) (4)hashtable_lookup(key) (5)hashtable_delete()	
+*/
+
+
